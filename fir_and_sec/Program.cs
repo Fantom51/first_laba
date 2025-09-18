b8b2508b94156c7ec817a815688af798911272c8
Fantom51 <79445595+Fantom51@users.noreply.github.com>
2025-09-18 15:11:19 +0300

    Первая лаба
    вроде полностью готова

---
+++ fir_and_sec/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fir_and_sec
+{
+    internal class Program
+    {
+        class Rayon
+        {
+            public int Population { get; set; }
+            public Rayon(int population)
+            {
+                Population = population;
+            }
+            public virtual  void Display()
+            {
+                Console.WriteLine("Население района: " + Population);
+                Console.WriteLine(" ");
+                
+            }
+
+        }
+        class Poselok : Rayon
+        {
+            public string Name { get; set; }
+            public Poselok(int population, string name) :base(population)
+            {
+                Name = name;
+            }
+            public override void Display()
+            {
+                Console.WriteLine("Население посёлка: " + Population);
+                Console.WriteLine("Название посёлка: " + Name);
+                Console.WriteLine(" ");
+            }
+        }
+        class City : Poselok
+        {
+            public double Area { get; set; }
+            public City(int population, string name, double area) : base(population, name)
+            {
+                Area = area;
+            }
+            public override void Display()
+            {
+                Console.WriteLine("Население города: " + Population);
+                Console.WriteLine("Название города: " + Name);
+                Console.WriteLine("Площадь города: " + Area);
+                Console.WriteLine(" ");
+            }
+        }
+        class Oblast : City
+        {
+            public string AdminCenter { set; get; }
+            public Oblast(int population, string name, double area, string admincenter) : base(population, name, area)
+            {
+                AdminCenter = admincenter;
+            }
+            public override void Display()
+            {
+                Console.WriteLine("Население области: " + Population);
+                Console.WriteLine("Название области: " + Name);
+                Console.WriteLine("Площадь области: " + Area);
+                Console.WriteLine("Административный центр области: " + AdminCenter);
+                Console.WriteLine(" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rayon ob1 = new Rayon(10000);
+            ob1.Display();
+            Rayon ob2 = new Poselok(100000,"Egoryevsk");
+            ob2.Display();
+            Rayon ob3 = new City(500000, "Egorsk", 5000.50);
+            ob3.Display();
+            Rayon ob4 = new Oblast(1000000, "Egoryevskaya oblast", 100000, "Egorsk");
+            ob4.Display();
+
+        }
+    }
+}
---
+++ fir_and_sec/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("fir_and_sec")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fir_and_sec")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe0fcc83-0223-4bf1-8bdf-60fba92b7426")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
